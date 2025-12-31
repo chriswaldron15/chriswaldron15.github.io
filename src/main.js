80ae8a709c25c9a42da2e4f2553b1f9a585c0756
chriswaldron15 <9429367+chriswaldron15@users.noreply.github.com>
2025-12-31 13:31:47 +0000

    remove full stop

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@
     let additionalYears = 3;    // 2 and a bit at Red7Mobile, 8 months at Raredrop Games
     let generalYears = 6;        // 2 at CGI, just under 4 at OVO energy
     
-    $("#tagline")[0].innerText = `Lead/ senior games programmer with over ${dateDiff.getFullYear() - 1970 + additionalYears} years professional game development experience and over ${dateDiff.getFullYear() - 1970 + additionalYears + generalYears}. years as a software engineer.`;
+    $("#tagline")[0].innerText = `Lead/ senior games programmer with over ${dateDiff.getFullYear() - 1970 + additionalYears} years professional game development experience and over ${dateDiff.getFullYear() - 1970 + additionalYears + generalYears} years as a software engineer.`;
     $("#employed_length")[0].innerText = new Date(dateNow - firstEmployedDate).getFullYear() - 1970;
 })
